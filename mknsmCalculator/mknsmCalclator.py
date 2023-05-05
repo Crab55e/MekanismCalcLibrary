9fc5fd5ebab7aa21714645c0acfe611c6647af8c
hihimamuLab <hihimamug14@gmail.com>
2023-05-05 22:08:50 +0900

    improved code

--- mknsmCalculator/mknsmCalclator.py
+++ mknsmCalculator/mknsmCalclator.py
@@ -4,12 +4,12 @@ def mknsmCalc(name, quantity):
             match quantity:
                 case quantity if quantity*4 > 64:
                     result = {
-                        "塩": quantity*4
+                        "塩": f"{int(quantity*4/64)}スタックと{quantity%64}個"
                     }
                     return result
                 case _:
                     result = {
-                        "塩": quantity*4
+                        "塩": f"{quantity*4}個"
                     }
                     return result
-        
+
