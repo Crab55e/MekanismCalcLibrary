dfe864fb68b36468732be07c6b9854a7e65550af
hihimamuLab <hihimamug14@gmail.com>
2023-05-05 21:48:20 +0900

    made template

--- mknsmCalculator/mknsmCalclator.py
+++ mknsmCalculator/mknsmCalclator.py
@@ -1,29 +1,15 @@
 def mknsmCalc(name, quantity):
-    result = {}
     match name:
         case "塩ブロック":
-            result = {
-                "塩": quantity*4
-            }
-            return result
-        case "木炭ブロック":
-            result = {
-                "木炭": quantity*9
-            }
-            return result
-        case "オスミウムブロック":
-            result = {
-                "オスミウム": quantit*9
-            }
-            return result
-        case "銅ブロック":
-            result = {
-                "銅": quantity*9
-            }
-            return result
-        case "錫ブロック":
-            result = {
-                "錫": quantity*9
-            }
-            return result
-
+            match quantity:
+                case quantity if quantity*4 > 64:
+                    result = {
+                        "塩": quantity*4
+                    }
+                    return result
+                case _:
+                    result = {
+                        "塩": quantity*4
+                    }
+                    return result
+        
