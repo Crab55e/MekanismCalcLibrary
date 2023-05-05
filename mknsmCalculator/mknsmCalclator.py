b01198633f0a3cd8cf8d1974d0f28417d58c37dc
hihimamuLab <hihimamug14@gmail.com>
2023-05-05 23:42:53 +0900

    improved code

--- mknsmCalculator/mknsmCalclator.py
+++ mknsmCalculator/mknsmCalclator.py
@@ -4,7 +4,7 @@ def mknsmCalc(name, quantity):
             match quantity:
                 case quantity if quantity*4 >= 64:
                     result = {
-                        "塩": f"{int(quantity*4/64)}スタックと{quantity*4-64*int(quantity*4/64)}個"
+                        "塩": f"{int(quantity*4/64)}スタックと{quantity*4%64}個"
                     }
                     return result
                 case _:
@@ -16,18 +16,110 @@ def mknsmCalc(name, quantity):
             match quantity:
                 case quantity if quantity*9 >= 64:
                     result = {
-                        "木炭": f"{int(quantity*9/64)}スタックと{quantity*9-64*int(quantity*9/64)}個"
+                        "木炭": f"{int(quantity*9/64)}スタックと{quantity*9%64}個"
                     }
                     return result
                 case _:
                     result = {
                         "木炭": f"{quantity*9}個"
                     }
+                    return result
+        case "銅ブロック":
+            match quantity:
+                case quantity if quantity*9 >= 64:
+                    result = {
+                        "銅インゴット": f"{int(quantity*9/64)}スタックと{quantity*9%64}個"
+                    }
+                    return result
+                case _:
+                    result = {
+                        "銅インゴット": f"{quantity*9}個"
+                    }
+                    return result
+        case "錫ブロック":
+            match quantity:
+                case quantity if quantity*9 >= 64:
+                    result = {
+                        "錫インゴット": f"{int(quantity*9/64)}スタックと{quantity*9%64}個"
+                    }
+                    return result
+                case _:
+                    result = {
+                        "錫インゴット": f"{quantity*9}個"
+                    }
+                    return result
+        case "鉛ブロック":
+            match quantity:
+                case quantity if quantity*9 >= 64:
+                    result = {
+                        "鉛インゴット": f"{int(quantity*9/64)}スタックと{quantity*9%64}個"
+                    }
+                    return result
+                case _:
+                    result = {
+                        "鉛インゴット": f"{quantity*9}個"
+                    }
+                    return result
+        case "ウランブロック":
+            match quantity:
+                case quantity if quantity*9 >= 64:
+                    result = {
+                        "ウランインゴット": f"{int(quantity*9/64)}スタックと{quantity*9%64}個"
+                    }
+                    return result
+                case _:
+                    result = {
+                        "ウランインゴット": f"{quantity*9}個"
+                    }
+                    return result
+        case "鋼鉄ブロック":
+            match quantity:
+                case quantity if quantity*9 >= 64:
+                    result = {
+                        "鋼鉄インゴット": f"{int(quantity*9/64)}スタックと{quantity*9%64}個"
+                    }
+                    return result
+                case _:
+                    result = {
+                        "鋼鉄インゴット": f"{quantity*9}個"
+                    }
+                    return result
+        case "青銅ブロック":
+            match quantity:
+                case quantity if quantity*9 >= 64:
+                    result = {
+                        "青銅インゴット": f"{int(quantity*9/64)}スタックと{quantity*9%64}個"
+                    }
+                    return result
+                case _:
+                    result = {
+                        "青銅インゴット": f"{quantity*9}個"
+                    }
+                    return result
+        case "精製黒曜石ブロック":
+            match quantity:
+                case quantity if quantity*9 >= 64:
+                    result = {
+                        "精製黒曜石インゴット": f"{int(quantity*9/64)}スタックと{quantity*9%64}個"
+                    }
+                    return result
+                case _:
+                    result = {
+                        "精製黒曜石インゴット": f"{quantity*9}個"
+                    }
+                    return result
+        case "精製グロウストーンブロック":
+            match quantity:
+                case quantity if quantity*9 >= 64:
+                    result = {
+                        "精製グロウストーンインゴット": f"{int(quantity*9/64)}スタックと{quantity*9%64}個"
+                    }
+                    return result
+                case _:
+                    result = {
+                        "精製グロウストーンインゴット": f"{quantity*9}個"
+                    }
+                    return result
         case _:
             return "有効な値を入力してください"
-        
-print(mknsmCalc("塩ブロック", 17))
-print(mknsmCalc("塩ブロック", 15))
-print(mknsmCalc("木炭ブロック", 17))
-print(mknsmCalc("木炭ブロック", 15))
-print(mknsmCalc("例外", 17))
+print(mknsmCalc("塩ブロック", 20))
