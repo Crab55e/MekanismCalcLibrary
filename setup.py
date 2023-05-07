e4a12a7a32405882af24a4ca322560d2aa6a8ea6
hihimamuLab <hihimamug14@gmail.com>
2023-05-07 14:02:37 +0900

    change

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="mknsmPy",
-    version="0.1",
-    packages=find_packages()
+    name         = "mknsmPy",
+    license      = "MIT",
+    version      = "0.1",
+    packages     = find_packages(),
+    author       = "hihimamu",
+    author_email = "mamu.pypi@shchiba.uk",
+    url          = "https://github.com/hihimamuLab/MekanismCalcLibrary.git",
+    description  = "mekanism material calculator",
+    keywords     = "mekanism", "mechanism", "mknsm",
+    classfiers   = "Programming Language :: Python :: 3.10"
 )
