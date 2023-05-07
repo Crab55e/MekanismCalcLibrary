201185a598ab09e169a4340698eadab98b8d718e
hihimamuLab <hihimamu14@outlook.jp>
2023-05-08 00:24:29 +0900

    edited version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst', encoding='utf-8') as f:
 setup(
     name                          = "mknsmPy", 
     license                       = "MIT",
-    version                       = "0.1", 
+    version                       = "0.2", 
     packages                      = find_packages(), 
     author                        = "hihimamu", 
     author_email                  = "mamu.pypi@shchiba.uk", 
