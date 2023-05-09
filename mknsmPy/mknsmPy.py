f41c8b4cbbc2f189648bbe2c504d0cd8a1b8d736
hihimamuLab <hihimamu14@outlook.jp>
2023-05-09 23:28:47 +0900

    fix code

--- mknsmPy/mknsmPy.py
+++ mknsmPy/mknsmPy.py
@@ -701,10 +701,10 @@ def Calc(name, quantity):
     if name == "攻撃力増強ユニット":
         if quantity*4 >= 64:
             return {
-                "吹込合金": f"{int(quantity*4/64)}ストックと{quantity*4%64}個",
-                "HDPEシート": f"{int(quantity*3/64)}ストックと{quantity*3%64}個",
-                "鉄の剣": f"{int(quantity/64)}ストックと{quantity%64}個",
-                "モジュールベース": f"{int(quantity/64)}ストックと{quantity%64}個"
+                "吹込合金": f"{int(quantity*4/64)}スタックと{quantity*4%64}個",
+                "HDPEシート": f"{int(quantity*3/64)}スタックと{quantity*3%64}個",
+                "鉄の剣": f"{int(quantity/64)}スタックと{quantity%64}個",
+                "モジュールベース": f"{int(quantity/64)}スタックと{quantity%64}個"
             }
         else:
             return {
@@ -716,9 +716,9 @@ def Calc(name, quantity):
     if name == "油圧式推進ユニット": 
         if quantity*3 >= 64:
             return {
-                "ポロニウムペレット": f"{int(quantity*3/64)}ストックと{quantity*3%64}個",
-                "強化合金": f"{int(quantity*2/64)}ストックと{quantity*2%64}個",
-                "エネルギータブレット": f"{int(quantity*2/64)}ストックと{quantity*2%64}個",
+                "ポロニウムペレット": f"{int(quantity*3/64)}スタックと{quantity*3%64}個",
+                "強化合金": f"{int(quantity*2/64)}スタックと{quantity*2%64}個",
+                "エネルギータブレット": f"{int(quantity*2/64)}スタックと{quantity*2%64}個",
                 "フリーランナー": f"{int(quantity/64)}スタックと{quantity%64}個",
                 "モジュールベース": f"{int(quantity/64)}スタックと{quantity%64}個"
             }
