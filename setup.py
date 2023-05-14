e01e49219bc54493309eb577541734b09542d561
hihimamuLab <hihimamu14@outlook.jp>
2023-05-14 21:58:28 +0900

    fixed code about version in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst', encoding='utf-8') as f:
 setup(
     name                          = "mknsmPy", 
     license                       = "MIT",
-    version                       = "0.3", 
+    version                       = "0.4", 
     packages                      = find_packages(), 
     author                        = "hihimamu", 
     author_email                  = "mamu.pypi@shchiba.uk", 
