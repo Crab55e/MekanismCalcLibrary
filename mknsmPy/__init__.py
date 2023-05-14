209242276d4e75100d767a5dc86fec405f139519
hihimamuLab <122292089+hihimamuLab@users.noreply.github.com>
2023-05-14 23:38:09 +0900

    Update __init__.py

--- mknsmPy/__init__.py
+++ mknsmPy/__init__.py
@@ -4,7 +4,7 @@ from mknsmPy.mb_materials import *
 
 __copyright__     = "Copyright (C) 2023 hihimamu"
 __license__       = "MIT" 
-__version__       = "0.4"
+__version__       = "1.0.0"
 __author__        = "hihimamu"
 __author_email__  = "mamu.pypi@shchiba.uk"
 __url__           = "https://github.com/hihimamuLab/MekanismCalcLibrary.git" 
