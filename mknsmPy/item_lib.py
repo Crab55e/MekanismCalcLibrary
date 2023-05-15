e52dda572ff68d154d0769df4b08de80d1536fa5
hihimamuLab <hihimamu14@outlook.jp>
2023-05-15 16:47:38 +0900

    fixed some code in ./mknsmPy/item_lib.py

--- mknsmPy/item_lib.py
+++ mknsmPy/item_lib.py
@@ -1,28 +1,15 @@
 class Lib:
     
-    lib = [
-        "a", "b", "c", "d", "e", 
-        "f", "g", "h", "i", "j", 
-        "k", "l", "m", "n", "o", 
-        "p", "q", "r", "s", "t", 
-        "u", "v", "w", "x", "y", "z", 
-    ]
-    def __init__(self,lib=lib):
-        
-        self.lib = lib
+    lib = []
+
+    def show(self):
+        return self.lib
     
-    def head(self, l=5):
-       
-        self.head = self.lib[:l]
-        return self.head
-        
-    def tail(self, l=5):
-
-        tail_len = int(len(self.lib)) - l
-        self.tail = self.lib[tail_len:]
-        return self.tail
-
-    def set(self, s=0, e=-1):
-
-        self.set = self.lib[s:e]
-        return self.set
+    def head(self, length = 5):
+        return self.lib[:length] 
+    
+    def tail(self, length = 5):
+        return self.lib[(int(len(self.lib)) - length):]
+    
+    def set(self, start, end):
+        return self.lib[start:end]
