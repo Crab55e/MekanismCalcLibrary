72e1d94e3892b7654e881132eae713d0fe3e4d96
hihimamuLab <122292089+hihimamuLab@users.noreply.github.com>
2023-05-15 15:44:59 +0900

    Update item_lib.py to test

--- mknsmPy/item_lib.py
+++ mknsmPy/item_lib.py
@@ -1,7 +1,14 @@
 class Lib:
     
-    lib = []
-
+    lib = [
+        "a", "b", "c", "d", "e", 
+        "f", "g", "h", "i", "j", 
+        "k", "l", "m", "n", "o", 
+        "p", "q", "r", "s", "t", 
+        "u", "v", "w", "x", "y", "z", 
+    ]
+    
+    return lib
     def __init__(self,lib=lib):
         
         self.lib = lib
